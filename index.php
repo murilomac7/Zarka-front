5e64855581c1d8f2496e6543c5bf6dc6ab5c1625
murilomac7 <murilomac7@gmail.com>
2023-05-27 23:56:44 -0300

    ajuste deploy vercel

--- index.php
+++
@@ -1,8 +0,0 @@
-<?php
-    $path = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
-    if ($path == '/painel') {
-        include_once 'painel.html';
-    } else {
-        include_once 'index.html';
-    }
-?>
