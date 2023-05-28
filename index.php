776589c952f9b523143839b4c1d5f756ee975617
murilomac7 <murilomac7@gmail.com>
2023-05-27 23:50:21 -0300

    ajuste deploy multiplas paginas

--- index.php
+++ index.php
@@ -1,1 +1,8 @@
-<?php include_once("index.html"); ?>
+<?php
+    $path = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+    if ($path == '/painel') {
+        include_once 'painel.html';
+    } else {
+        include_once 'index.html';
+    }
+?>
